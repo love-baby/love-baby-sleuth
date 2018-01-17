00df7356b9a2a1a589626e39a6984cef33224f7e
liangbc <bicong.liang@51zouchuqu.com>
2018-01-17 17:49:03 +0800

    初始化

--- src/main/java/com.love.baby.sleuth/SleuthApplication.java
+++ src/main/java/com.love.baby.sleuth/SleuthApplication.java
@@ -3,13 +3,13 @@ package com.love.baby.sleuth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.EnableZipkinServer;
 
 /**
  * Created by liangbc on 2017/7/25.
  */
-@EnableDiscoveryClient
 @EnableAutoConfiguration
+@EnableZipkinServer
 @SpringBootApplication(scanBasePackages = {"com.love"})
 public class SleuthApplication {
     public static void main(String[] args) {
