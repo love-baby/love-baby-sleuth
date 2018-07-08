1d218933f5481622770e1bfe5103cad13cbb0b65
liangbc <bicong.liang@51zouchuqu.com>
2018-07-08 19:12:45 +0800

    密码

--- src/main/java/com.love.baby.sleuth/SleuthApplication.java
+++ src/main/java/com.love.baby.sleuth/SleuthApplication.java
@@ -1,10 +1,8 @@
 package com.love.baby.sleuth;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 import zipkin.server.internal.EnableZipkinServer;
 
 /**
@@ -12,8 +10,6 @@ import zipkin.server.internal.EnableZipkinServer;
  * @author liangbc
  * @date 2017/7/25
  */
-@Configuration
-@EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableZipkinServer
 @SpringCloudApplication
