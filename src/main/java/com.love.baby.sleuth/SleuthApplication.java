eab495f3f64c5e784186742870119b391777f66a
liangbc <bicong.liang@51zouchuqu.com>
2018-07-08 18:26:44 +0800

    密码

--- src/main/java/com.love.baby.sleuth/SleuthApplication.java
+++ src/main/java/com.love.baby.sleuth/SleuthApplication.java
@@ -2,16 +2,20 @@ package com.love.baby.sleuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 /**
- * Created by liangbc on 2017/7/25.
+ *
+ * @author liangbc
+ * @date 2017/7/25
  */
+@Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @zipkin.server.internal.EnableZipkinServer
-@SpringBootApplication
+@SpringCloudApplication
 public class SleuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(SleuthApplication.class, args);
