bba7ead716a2b2a3487b65d3962aa8d311f21a2c
liangbc <bicong.liang@51zouchuqu.com>
2018-09-08 19:42:24 +0800

    Mq

--- src/main/java/com.love.baby.sleuth/SleuthApplication.java
+++ src/main/java/com.love.baby.sleuth/SleuthApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import zipkin.server.internal.EnableZipkinServer;
+import zipkin2.server.internal.EnableZipkinServer;
 
 /**
  * @author liangbc
